a57f08eb1889e215c7866e805534e2689c00dd2d
mahmoud-saber <mahmoudsabersd.12345@gmil.com>
2025-12-02 00:32:44 +0200

    create structures files of project

---
+++ app/Http/Controllers/HoldController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Hold;
+use Illuminate\Http\Request;
+
+class HoldController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Hold $hold)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Hold $hold)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Hold $hold)
+    {
+        //
+    }
+}
